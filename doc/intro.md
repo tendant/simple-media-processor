71dac62b9caf40030a88462b355ef1129fcd163c
Lei Wang <tendant@gmail.com>
2018-05-15 11:36:26 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-media-prcoessor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simple_media_prcoessor/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-media-prcoessor.core-test
+  (:require [clojure.test :refer :all]
+            [simple-media-prcoessor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
