774dfb632d379602937d16ee7d616f1cbac29605
Lei Wang <tendant@gmail.com>
2018-05-15 13:09:09 -0700

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-media-prcoessor "0.1.0-SNAPSHOT"
+(defproject simple-media-prcoessor "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
