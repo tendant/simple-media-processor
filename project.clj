ddb4cc6a60d00de0053f2675373ee60a7853a55c
Yangye Zhu <yangye.zhu@wishlife.com>
2018-05-17 11:18:27 -0700

    update to 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-media-prcoessor "0.1.2"
+(defproject simple-media-prcoessor "0.1.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
