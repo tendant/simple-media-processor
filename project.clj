3f807e278820c0abaf9ec8d4eaf72806e1287de5
Yangye Zhu <yangye.zhu@wishlife.com>
2020-02-05 13:55:37 -0800

    release 0.1.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-media-prcoessor "0.1.12"
+(defproject simple-media-prcoessor "0.1.13"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
