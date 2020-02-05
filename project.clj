7bd3a870e4f0336897eb7ed505aef10d44ab4612
Lei Wang <tendant@gmail.com>
2020-02-05 13:59:42 -0800

    release 0.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-media-prcoessor "0.1.13"
+(defproject simple-media-prcoessor "0.1.14"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
